cb2f1d8cb97c1908246266634c17b4fa175eb1e9
WalterSanmiguel <107519708+WalterSanmiguel@users.noreply.github.com>
2022-06-17 17:46:07 +1200

    hice una unidor de strings

---
+++ nombre.js
@@ -0,0 +1,6 @@
+function miNombreCompleto(){
+var nombre = document.getElementById("nombre").value;
+var apellido = document.getElementById("apellido").value;
+var union = nombre +" "+ apellido
+var parrafocito = document.getElementById("parrafo").innerText = union
+}
